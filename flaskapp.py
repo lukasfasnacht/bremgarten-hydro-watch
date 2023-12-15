07fca6e873dc0c5d79b6301e7f040446f4ef38ef
Lukas Fasancht <lukas.fasnacht@gmail.com>
2023-12-15 16:17:47 +0100

    filter function does now have option for equal or equal-greater

--- flaskapp.py
+++ flaskapp.py
@@ -9,25 +9,31 @@ def anzeigen():
     connection = sqlite3.connect('hydrodata.db')
     cursor = connection.cursor()
 
-    # Filter-Wert abrufen (wird 'None' sein, wenn nicht im Formular gepostet)
+    # Filter-Wert und Vergleichsoperator abrufen
     abfluss_filter = request.form.get('abfluss_filter')
+    comparison_operator = request.form.get('comparison_operator', 'exact')  # Standard: genaue Suche
 
     # Filter zurücksetzen, wenn der entsprechende Button geklickt wurde
     if 'reset_filter' in request.form:
         abfluss_filter = None
+        comparison_operator = 'comparison_operator'
 
     # Daten aus der Tabelle abrufen und absteigend nach der ID sortieren
     if abfluss_filter is not None:
-        cursor.execute('SELECT * FROM reuss WHERE abfluss >= ? ORDER BY id DESC', (abfluss_filter,))
+        if comparison_operator == 'exact':
+            cursor.execute('SELECT * FROM reuss WHERE abfluss = ? ORDER BY id DESC', (abfluss_filter,))
+        elif comparison_operator == 'greater_equal':
+            cursor.execute('SELECT * FROM reuss WHERE abfluss >= ? ORDER BY id DESC', (abfluss_filter,))
     else:
         cursor.execute('SELECT * FROM reuss ORDER BY id DESC')
-    daten = cursor.fetchall()
 
+    daten = cursor.fetchall()
+    
     # Verbindung schließen
     connection.close()
 
     # HTML-Template rendern und Daten hergeben
-    return render_template('index.html', daten=daten, abfluss_filter=abfluss_filter)
+    return render_template('index.html', daten=daten, abfluss_filter=abfluss_filter, comparison_operator=comparison_operator)
 
 if __name__ == '__main__':
     app.run(debug=True)
